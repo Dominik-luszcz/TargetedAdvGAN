6c90c2fe688d9994a89116afa4b09d23dff54176
Dominik-luszcz <luszczynski.dominik@gmail.com>
2025-06-18 09:12:48 -0400

    Update Week 6 progress

---
+++ GANS/DCGAN/dc_gan.py
@@ -0,0 +1,289 @@
+import pandas as pd
+import numpy as np
+import torch
+from torch import Tensor
+import pytorch_lightning as pl
+import torch.nn as nn
+from sub_models import *
+from datetime import datetime
+import matplotlib.pyplot as plt
+from scipy.stats import wasserstein_distance_nd
+from torch.utils.data import DataLoader
+import random
+import os
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+class DCGAN(pl.LightningModule):
+    def __init__(self, generator_hidden_dim, noise_dim, generator_output_dim, discriminator_hidden_dim,
+                 sample_size,
+                 scale_max = None, scale_min = None, lr=0.0001, plot_paths='.'):
+        super().__init__()
+
+        self.lr = lr
+
+        self.generator_hidden_dim = generator_hidden_dim
+        self.noise_dim = noise_dim
+        self.output_dim = generator_output_dim
+        self.discriminator_hidden_dim = discriminator_hidden_dim
+        self.plot_paths = plot_paths
+        self.scale_max = scale_max
+        self.scale_min = scale_min
+        self.sample_size = sample_size
+
+        self.generator = Generator(output_dim=generator_output_dim, sample_size=sample_size)
+        self.discriminator = Discriminator(input_dim=1, hidden_dim=discriminator_hidden_dim, output_dim=1)
+
+        self.automatic_optimization = False
+
+        self.discriminator_losses_real = []
+        self.discriminator_losses_fake = []
+        self.generator_losses = []
+        self.batch_sizes = []
+        self.w_dist = []
+        self.example_outputs = []
+
+        self.d_loss_real = []
+        self.d_loss_fake = []
+        self.g_loss = []
+
+        self.save_hyperparameters()
+
+
+    def training_step(self, batch):
+        # get the optimizers for the generator and discriminator
+        generator_optimizer, discriminator_optimizer = self.optimizers()
+
+        # batch would give me batches of real data
+        real_data = batch
+        real_data = real_data.unsqueeze(-1).to(DEVICE)
+        b, seq_len, dim = real_data.shape
+        real_data_labels = torch.ones((b, 1), dtype=torch.float64, device=DEVICE) - 0.15
+        fake_data_labels = torch.zeros((b, 1), dtype=torch.float64, device=DEVICE) + 0.15
+
+        # Step 1: we need to train the discriminator
+        for p in self.discriminator.parameters():
+            p.requires_grad = True
+        discriminator_optimizer.zero_grad()
+
+        # First we need to train on the real data
+        discriminator_output_real = self.discriminator(real_data)
+        # because we classify per ticker/recording we take the average to get size b x 1
+        discriminator_output_real = discriminator_output_real.mean(dim=1)
+        discriminator_loss_real = nn.functional.binary_cross_entropy(discriminator_output_real, real_data_labels)
+        discriminator_loss_real.backward()
+
+        # Next we need to train the discriminator on the fake data
+        z = torch.randn(b, self.noise_dim, dtype=torch.float64, device=DEVICE).unsqueeze(-1)
+        fake_data = self.generator(z)
+        discriminator_output_fake = self.discriminator(fake_data.detach())
+        discriminator_output_fake = discriminator_output_fake.mean(dim=1)
+        discriminator_loss_fake = nn.functional.binary_cross_entropy(discriminator_output_fake, fake_data_labels)
+        discriminator_loss_fake.backward()
+
+        self.discriminator_losses_real.append( discriminator_loss_real.detach())
+        self.discriminator_losses_fake.append( discriminator_loss_fake.detach())
+
+        discriminator_optimizer.step()
+
+        # Step 2: Train the generator
+        for p in self.discriminator.parameters():
+            p.requires_grad = False
+
+        generator_optimizer.zero_grad()
+        z = torch.randn(b, self.noise_dim, dtype=torch.float64, device=DEVICE).unsqueeze(-1)
+        fake_data = self.generator(z)
+        discriminator_output_fake = self.discriminator(fake_data)
+        discriminator_output_fake = discriminator_output_fake.mean(dim=1)
+        # Now we want to update the generator gradients to make our fake data be classified as real data 
+        # mean = torch.mean(fake_data, dim=1)
+        # stdev = torch.std(fake_data, dim=1)
+        # kurtosis = torch.sum(((fake_data - mean[:, None]) / stdev[:, None]) ** 4, dim=1).mean(dim=1)
+        # fake_kurtosis = kurtosis.mean()
+
+        # mean = torch.mean(real_data, dim=1)
+        # stdev = torch.std(real_data, dim=1)
+        # kurtosis = torch.sum(((real_data - mean[:, None]) / stdev[:, None]) ** 4, dim=1).mean(dim=1)
+        # real_kurtosis = kurtosis.mean()
+
+        # k_loss = torch.abs(real_kurtosis - fake_kurtosis)
+
+        generator_loss = nn.functional.binary_cross_entropy(discriminator_output_fake, real_data_labels + 0.15) 
+
+        generator_loss.backward()
+
+        # generator_grad = 0
+        # for param in self.generator.parameters():
+        #     if param.grad is not None:
+        #         generator_grad += param.grad.abs().sum().item()
+        
+        # print(generator_grad)
+        # fake_data = self.generator(z)
+
+        # mean = torch.mean(fake_data, dim=1)
+        # stdev = torch.std(fake_data, dim=1)
+        # kurtosis = torch.sum(((fake_data - mean[:, None]) / stdev[:, None]) ** 4, dim=1).mean(dim=1)
+        # fake_kurtosis = kurtosis.mean()
+
+        # mean = torch.mean(real_data, dim=1)
+        # stdev = torch.std(real_data, dim=1)
+        # kurtosis = torch.sum(((real_data - mean[:, None]) / stdev[:, None]) ** 4, dim=1).mean(dim=1)
+        # real_kurtosis = kurtosis.mean()
+
+        # k_loss = torch.abs(real_kurtosis - fake_kurtosis)
+        # k_loss.backward()
+
+
+        # Maybe try a sort of stdev loss
+
+        #stdev_loss = fake_stdev / (real_stdev + 1e-06) + real_stdev / (real_stdev + 1e-06)
+        #stdev_loss.backward()
+        generator_optimizer.step()
+
+        self.generator_losses.append(generator_loss.detach())
+        self.batch_sizes.append(b)
+
+        #self.log("wass_dist", wass_dist, on_step=False, on_epoch=True, batch_size=b)
+    
+    def forward(self, x):
+        return self.generator(x)
+
+    
+    def configure_optimizers(self):
+        generator_optimizer = torch.optim.Adam(self.generator.parameters(), lr=1e-03, betas=(0.5, 0.999))
+        discriminator_optimizer = torch.optim.Adam(self.discriminator.parameters(), lr=1e-05, betas=(0.5, 0.999))
+
+        return generator_optimizer, discriminator_optimizer
+    
+
+class DCGAN_Callback(pl.Callback):
+    def __init__(self, log_returns):
+        super().__init__()
+        self.log_returns = log_returns
+
+    def on_train_epoch_end(self, trainer: pl.Trainer, model: DCGAN):
+
+        generator_losses = torch.Tensor(model.generator_losses)
+        discriminator_losses_real = torch.Tensor(model.discriminator_losses_real)
+        discriminator_losses_fake = torch.Tensor(model.discriminator_losses_fake)
+        batch_sizes = torch.Tensor(model.batch_sizes)
+
+        d_loss_real = sum(discriminator_losses_real *  batch_sizes) / sum(batch_sizes)
+        d_loss_fake = sum(discriminator_losses_fake *  batch_sizes) / sum(batch_sizes)
+        g_loss = sum(generator_losses *  batch_sizes) / sum(batch_sizes)
+
+        # make epoch dir
+        os.makedirs(f'{model.plot_paths}/Epoch_{model.current_epoch}')
+        os.chmod(f'{model.plot_paths}/Epoch_{model.current_epoch}', 0o700)
+        
+
+        # 1. Sample n intervals of 400 days and compute stats like kurtosis and skew
+        num_to_sample = 16
+        real_means = []
+        real_stdevs = []
+        real_iqr = []
+        real_skew = []
+        real_kurtosis = []
+        for i in range(num_to_sample):
+            start = random.randint(0, len(self.log_returns) - model.sample_size)
+            interval = self.log_returns[start : start + model.sample_size]
+            if i == 0:
+                plt.plot(interval.numpy())
+                plt.xlabel('Time (days)')
+                plt.ylabel('Log Returns')
+                plt.title(f'Example real Return: {model.sample_size} Days ')
+                plt.savefig(f'{model.plot_paths}/Epoch_{model.current_epoch}/example_real_output_epoch{model.current_epoch}.png')
+                plt.close()
+
+            mean = torch.mean(interval)
+            stdev = torch.std(interval)
+            q75 = torch.quantile(interval, q=0.75)
+            q25 = torch.quantile(interval, q=0.25)
+            iqr = q75 - q25
+
+            skew = torch.sum(((interval - mean) / stdev) ** 3) / model.sample_size
+            kurtosis = torch.sum(((interval - mean) / stdev) ** 4) / model.sample_size
+
+            real_means.append(mean)
+            real_stdevs.append(stdev)
+            real_iqr.append(iqr)
+            real_skew.append(skew)
+            real_kurtosis.append(kurtosis)
+        
+        real_means = torch.stack(real_means).mean()
+        real_stdevs = torch.stack(real_stdevs).mean()
+        real_iqr = torch.stack(real_iqr).mean()
+        real_skew = torch.stack(real_skew).mean()
+        real_kurtosis = torch.stack(real_kurtosis).mean()
+        
+       
+        model.eval()
+        with torch.no_grad():
+            z = torch.randn(num_to_sample, model.noise_dim, dtype=torch.float64).unsqueeze(-1)
+            fake_output = model.generator(z)
+
+            # Need to scale back to log returns
+            if model.scale_max != None and model.scale_min != None:
+
+                fake_output = (model.scale_max - model.scale_min) * ((fake_output + 1)/2) + model.scale_min
+
+            model.example_outputs.append(fake_output)
+
+        mean = torch.mean(fake_output, dim=1)
+        stdev = torch.std(fake_output, dim=1)
+        q75 = torch.quantile(fake_output, q=0.75, dim=1)
+        q25 = torch.quantile(fake_output, q=0.25, dim=1)
+        iqr = q75 - q25
+
+        skew = torch.sum(((fake_output - mean[:, None]) / stdev[:, None]) ** 3, dim=1).mean(dim=1)
+        kurtosis = torch.sum(((fake_output - mean[:, None]) / stdev[:, None]) ** 4, dim=1).mean(dim=1)
+
+        fake_means = mean.mean()
+        fake_stdevs = stdev.mean()
+        fake_iqr = iqr.mean()
+        fake_skew = skew.mean()
+        fake_kurtosis = kurtosis.mean()
+
+        loss = ((real_means - fake_means) ** 2 + (real_stdevs - fake_stdevs) ** 2 + 
+                (real_skew - fake_skew) ** 2 + (real_kurtosis - fake_kurtosis) ** 2)
+
+
+        with open(f"{model.plot_paths}/Epoch_{model.current_epoch}/generated_stats_epoch_{model.current_epoch}.txt", 'a') as file:
+            file.write("=" * 50 + "\n")
+            file.write(f"Real Mean: {real_means}, Fake Mean: {fake_means}\n")
+            file.write(f"Real Stdev: {real_stdevs}, Fake Stdev: {fake_stdevs}\n")
+            file.write(f"Real iqr: {real_iqr}, Fake iqr: {fake_iqr}\n")
+            file.write(f"Real skew: {real_skew}, Fake skew: {fake_skew}\n")
+            file.write(f"Real kurtosis: {real_kurtosis}, Fake kurtosis: {fake_kurtosis}\n")
+            file.write(f"Loss {loss}\n")
+            file.write("=" * 50 + "\n")
+
+        model.log('loss', loss)
+        model.train()
+        # plot example fake data
+        # first we need to scale the fake data to match that of the log returns
+        fake_output = fake_output.squeeze(-1).squeeze(0).detach()[0]
+        plt.plot(fake_output.numpy())
+        plt.xlabel('Time (days)')
+        plt.ylabel('Log Returns')
+        plt.title('Example log return created from GAN')
+        plt.savefig(f'{model.plot_paths}/Epoch_{model.current_epoch}/example_gan_output_epoch{model.current_epoch}.png')
+        plt.close()
+
+
+        print('=================================')
+        print(f"Discriminator Real Loss: {d_loss_real}")
+        print(f"Discriminator Fake Loss: {d_loss_fake}")
+        print(f"Generator Loss: {g_loss}")
+        print('=================================')
+
+        model.d_loss_fake.append(d_loss_fake)
+        model.d_loss_real.append(d_loss_real)
+        model.g_loss.append(g_loss)
+
+        model.w_dist = []
+        model.batch_sizes = []
+        model.discriminator_losses_fake = []
+        model.discriminator_losses_real = []
+        model.generator_losses = []
--- GANS/DCGAN/run_model.py
+++ GANS/DCGAN/run_model.py
@@ -103,8 +103,8 @@ class StockDataset(Dataset):
     def __getitem__(self, index):
         # Rather than the whole recording, subsample a random 500 days
         if self.mode == "train":
-            # start = random.randint(0, len(self.training_data[index]) - 500)
-            # return self.training_data[index][start : start + 500]
+            start = random.randint(0, len(self.training_data[index]) - 500)
+            return self.training_data[index][start : start + 500]
             return self.training_data[index]
         elif self.mode == "val":
             return self.validation_data[index]
@@ -112,10 +112,54 @@ class StockDataset(Dataset):
             return self.testing_data[index]
 
 
+class SingleStockDataset(Dataset):
+    def __init__(self, stock_folder, ticker, num_samples, sample_size):
+        super().__init__()
+ 
+
+        self.max_return = None
+        self.min_return = None
+        self.num_samples = num_samples
+        self.sample_size = sample_size
+
+        try:
+            self.training_stock = pd.read_csv(f"{stock_folder}/{ticker}.csv")['adjprc'].to_numpy()
+        except:
+            raise ValueError(f"The ticker {ticker} is not in the provided stock folder {stock_folder}")
+
+        self.log_returns = torch.from_numpy(np.log(self.training_stock[1:] / self.training_stock[:-1]))
+
+
+        self.unscaled_returns = self.log_returns
 
 
 
-def train(data_files, training_split_file, batch_size, num_epochs, output_path, noise_dim,
+
+        #scale the training data between -1 and 1
+        # self.max_return = torch.quantile(self.log_returns, 1)
+        # self.min_return = torch.quantile(self.log_returns, 0)
+
+        # print(f"Max Return: {self.max_return}")
+        # print(f"Min Return: {self.min_return}")
+
+        
+
+        # self.log_returns = 2 * ((self.log_returns - self.min_return) / (self.max_return - self.min_return)) - 1
+
+        
+            
+    def __len__(self):
+        return self.num_samples
+    
+    def __getitem__(self, index):
+        start = random.randint(0, len(self.log_returns) - self.sample_size)
+        return self.log_returns[start : start + self.sample_size]
+
+
+
+
+
+def train_on_all_stocks(data_files, training_split_file, batch_size, num_epochs, output_path, noise_dim,
            generator_hidden_dim, generator_output_dim, discriminator_hidden_dim, lr=0.0001):
 
     dataset = StockDataset(data_files, training_split_file=training_split_file, mode='train')
@@ -127,7 +171,7 @@ def train(data_files, training_split_file, batch_size, num_epochs, output_path, 
                        scale_max = dataset.max_return, scale_min = dataset.min_return)
     
     train_callback = ModelCheckpoint(
-            monitor="wass_dist",
+            monitor="loss",
             mode="min",
             save_top_k=1,
             filename=f"best-model",
@@ -137,6 +181,50 @@ def train(data_files, training_split_file, batch_size, num_epochs, output_path, 
     
     # Init the trainer
     trainer = pl.Trainer(devices='auto', accelerator='auto', accumulate_grad_batches=1, logger=False, callbacks=[train_callback], 
+                         num_sanity_val_steps=0, enable_checkpointing=True, max_epochs=num_epochs,#max_steps=MAX_ITERATIONS,
+                        enable_progress_bar=True, max_time='00:03:00:00', default_root_dir=output_path)
+    
+    trainer.fit(model, dataloader)
+
+    plot_loss_functions(model, output_path)
+
+    torch.save(model.state_dict(), f"{output_path}/dc_gan_model.pth")
+
+
+    #model.load_state_dict(torch.load('vGan_model.pth'))
+    #test_datset = StockDataset(data_files, training_split_file=training_split_file, mode='test')
+
+    # best_model_path = trainer.checkpoint_callback.best_model_path
+    # print(best_model_path)
+    # best_model = VanillaGAN.load_from_checkpoint(best_model_path)
+    # torch.save(best_model.state_dict(), f"dc_gan.pt")
+
+    #test_gan(model, noise_dim)
+
+    return
+
+def train_on_one_stocks(data_files, ticker, num_samples, sample_size, batch_size, num_epochs, output_path, noise_dim,
+           generator_hidden_dim, generator_output_dim, discriminator_hidden_dim, lr=0.0001):
+
+    dataset = SingleStockDataset(stock_folder=data_files, ticker=ticker, num_samples=num_samples, sample_size=sample_size)
+
+    dataloader = DataLoader(dataset, batch_size=batch_size)#, num_workers=19)
+
+    model = DCGAN(noise_dim=noise_dim, generator_hidden_dim=generator_hidden_dim, generator_output_dim=generator_output_dim, 
+                       discriminator_hidden_dim=discriminator_hidden_dim, lr=lr, plot_paths=output_path,
+                       scale_max = dataset.max_return, scale_min = dataset.min_return, sample_size=sample_size)
+    
+    train_callback = ModelCheckpoint(
+            monitor="loss",
+            mode="min",
+            save_top_k=1,
+            filename=f"best-model",
+            verbose=True,
+            dirpath=output_path
+    )
+    
+    # Init the trainer
+    trainer = pl.Trainer(devices='auto', accelerator='auto', accumulate_grad_batches=1, logger=False, callbacks=[train_callback, DCGAN_Callback(dataset.unscaled_returns)], 
                          num_sanity_val_steps=0, enable_checkpointing=True, max_epochs=num_epochs,#max_steps=MAX_ITERATIONS,
                         enable_progress_bar=True, max_time='00:03:00:00', default_root_dir=output_path)
     
@@ -194,11 +282,11 @@ if __name__ == '__main__':
     t1 = datetime.now()
     print(f"Started job at {t1}")
 
-    output_path = 'DCGan_Ouptuts2'
-    initialize_directory(output_path)
+    output_path = 'DCGan_A_noscale3'
+    initialize_directory(output_path) 
 
-    train(data_files="SP500_Filtered", training_split_file='training_split.npy', batch_size=16, #32 for subsample
-          num_epochs=100, output_path=output_path, noise_dim=64,
+    train_on_one_stocks(data_files="SP500_Filtered", ticker='A', num_samples=400, sample_size=400, batch_size=16, #32 for subsample
+          num_epochs=50, output_path=output_path, noise_dim=64,
           generator_hidden_dim=64, generator_output_dim=1, discriminator_hidden_dim=64)
 
     t2 = datetime.now()
